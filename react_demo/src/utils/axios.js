a333ca6705246e555b228c8cf5a7cc1b786fe063
LWK595 <54047996+LWK595@users.noreply.github.com>
2020-01-08 22:19:34 +0800

    axios

--- react_demo/src/utils/axios.js
+++ react_demo/src/utils/axios.js
@@ -2,38 +2,25 @@ import axios from "axios"
 // import {getItem} from './webStorage'
 import store from  '../store/store'
 import ActionCreator from  '../store/actionCreator'
-<<<<<<< HEAD
-axios.interceptors.request.use(function (config) {
-  // Do something before request is sent
-  //从缓存获取token 添加
-  // config.data.token=getItem('token')
-  // console.log(config)
-  config.data.token=getItem('token')||''
-//   if(localStorage.getItem('token')){
-//     axios.defaults.headers.commom['token']=localStorage.getItem('token')
-// }
-    
-  console.log(config)
-  return config;
-}, function (error) {
-  // Do something with request error
-  return Promise.reject(error);
-});
-  
-=======
+
 // axios.interceptors.request.use(function (config) {
 //   // Do something before request is sent
 //   //从缓存获取token 添加
+//   // config.data.token=getItem('token')
+//   // console.log(config)
 //   config.data.token=getItem('token')||''
+// //   if(localStorage.getItem('token')){
+// //     axios.defaults.headers.commom['token']=localStorage.getItem('token')
+// // }
+    
 //   console.log(config)
 //   return config;
 // }, function (error) {
 //   // Do something with request error
 //   return Promise.reject(error);
 // });
+  
 
-
->>>>>>> liwenke
 // Add a response interceptor
 axios.interceptors.response.use(function (response) {
   // Do something with response data
