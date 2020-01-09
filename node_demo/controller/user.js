e5420b08fefd9592f77dba2097eece7aad20c38e
BK1917yuyue <1429873028@qq.com>
2020-01-09 17:29:22 +0800

    123

--- node_demo/controller/user.js
+++
@@ -1,7 +0,0 @@
-const InformationModel= require('../db/model/userModel')
-async function add(us,ps){
-  // async 函数内部只要不出错 肯定走的是then 如果出错走的是catch
-   let result =await InformationModel.insertMany({us,ps})
-   console.log(result)
-}
-module.exports={add}
