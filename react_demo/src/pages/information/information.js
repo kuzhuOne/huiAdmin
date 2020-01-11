19ff5ab074155a75d9e8e8427926c2ce049e784e
BK1917caoJianRong <1850125484@qq.com>
2020-01-11 10:29:35 +0800

    cao

--- react_demo/src/pages/information/information.js
+++ react_demo/src/pages/information/information.js
@@ -4,13 +4,22 @@ import Search from "../../components/search"
 import List from "../../components/informationList"
 
 class Information extends Component{
+ constructor(){
+    super() //执行父类的构造函数
+    this.state={
+      infoCount:""
+    }
+  }
+  setInfoCount(params){
+  this.setState({infoCount:params})
+  }
     render(){
         return (
             <div>
                 资讯页面
                <Search></Search>
-               <Add></Add>
-               <List></List>
+               <Add info={this.state.infoCount}></Add>
+               <List info={this.setInfoCount.bind(this)}></List>
                
             </div>
         )
